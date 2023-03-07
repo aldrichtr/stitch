74c5ffeee7dadfb9cc6f96b40a6c579c4d2386a1
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2023-03-07 18:55:53 -0500

    feat: Add a logo to the output

---
+++ source/stitch/BuildScripts/Output/stitchLogo.build.ps1
@@ -0,0 +1,21 @@
+$stitchLogoSmall = @"
+   ___  _    _  _        _
+  / __|| |_ (_)| |_  __ | |_
+  \__ \|  _|| ||  _|/ _||   \
+  |___/ \__||_| \__|\__||_||_|
+"@
+
+
+$stitchLogoLarge = @'
+-=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=
+=-                                                                           =-
+-=          xxxxxx    xx        xxxx    xx                 xx                -=
+=-         x      x  x  x      x    x  x  x               x  x               =-
+-=        x    xxxx  x  xxxxx   xxxx   x  xxxxx    xxxx   x   xxx            -=
+=-        x      x   x      x   xxxx   x      x   x    x  x      x           =-
+-=         xxxx   x  x    xxx   x  x   x    xxx  x   xxx  x       x          -=
+=-         x      x   x     x   x  x    x     x  x     x  x  x  x  x         =-
+-=          xxxxxxx    xxxxxx   xxxx     xxxxxx   xxxxxx  xxxx  xxxx         -=
+=-                                                                           =-
+-=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=--=-=
+'@
