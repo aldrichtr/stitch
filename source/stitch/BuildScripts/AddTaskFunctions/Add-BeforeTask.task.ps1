b700ec109625460e52ee745c05c3ea3926b76b38
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2023-12-19 18:53:54 -0500

    fix: before and after task modify jobs list
    - Rather than putting tasks into the `Before` and `After` array, only to
      have Invoke-Build move them to the task list,
      functions now modify the job list directly

--- source/stitch/BuildScripts/AddTaskFunctions/Add-BeforeTask.task.ps1
+++ source/stitch/BuildScripts/AddTaskFunctions/Add-BeforeTask.task.ps1
@@ -1,64 +1,78 @@
 
-Set-Alias before Add-BeforeTask
 function Add-BeforeTask {
     <#
-.SYNOPSIS
-    Add tasks to run before another without needing to modify the original task
-.EXAMPLE
-    Build | before Package
+    .SYNOPSIS
+        Add tasks to run before another without needing to modify the original task
+    .EXAMPLE
+        Build | before Validate
 
-    This will ensure that the build task is run before the package
+        "Run 'Build' before 'Validate'"
+        This will ensure that the Build task is run before the Validate task
 
-.EXAMPLE
-    before Package Clean, Build
-#>
+    .EXAMPLE
+        Build | before Validate
+        Jobs    | before Task
+        Validate.Jobs = @( 'Build' , ... )
+    #>
+    [Alias('before')]
+    [CmdletBinding()]
     param(
-        # The primary task to add the TaskList to the 'Before' array
+        # The primary task. Jobs will be added to the end of this Task.
         [Parameter(
             Mandatory,
             Position = 0
-        )]
-        [string]$Name,
+        )][string[]]$Task,
 
-        # The task(s) that will be run before the task Name
+        # The job(s) that will be run before the Task
         [Parameter(
             Mandatory,
             Position = 1,
             ValueFromPipeline
-        )]
-        [string[]]$TaskList
+        )][string[]]$Jobs
 
     )
     begin {
         Write-Debug "`n$('-' * 80)`n-- Begin $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
+        $allTasks     = ${*}.All.Keys
+        $errorMessage = "Could not add {0} before {1}. '{2}' is not a valid task name"
+        #! Create a new list to replace the Jobs list with before processing
     }
     process {
-        $allTaskNames = ${*}.All.Keys
-        $errorMessage = "Could not add {0} before {1}. '{2}' in not a valid task name"
+        if ($null -ne $allTasks) {
+            foreach ($currentTask in $Task) {
+                # Normalize the name of the Task
+                $taskName   = $currentTask | Resolve-TaskName
+                #! Start with the current list of Jobs
 
-        if ($allTaskNames -contains $Name) {
-            Write-Debug "$Name is a valid task"
-            foreach ($item in $TaskList) {
-                if ($allTaskNames -contains $item) {
-                    Write-Debug "Adding $item to Before list of $Name"
+                if ($allTasks -contains $taskName) {
+                    foreach ($currentJob in $Jobs) {
+                        $jobName = $currentJob | Resolve-TaskName
 
-                    ${*}.All[$item].Before += $Name
+                        # if The current job is a valid task
+                        if ($allTasks -contains $jobName) {
+                            # if it is not already in the job list for this task
+                            if (${*}.All[$taskName].Jobs -notcontains $currentJob) {
+                                Write-Debug "Adding $currentJob to beginning of $taskName Jobs"
+                                ${*}.All[$taskName].Jobs.Insert(0,$currentJob)
+                            } else {
+                                Write-Verbose "$currentJob already a job of $currentTask"
+                            }
+                        } else {
+                            throw ($errorMessage -f $currentJob, $currentTask, $currentJob)
+                        }
+                    }
                 } else {
-                    throw ($errorMessage -f $item, $Name, $item)
+                    throw ($errorMessage -f $currentJob, $currentTask, $currentTask)
+                }
+
+                #! If we are in the pipeline, send out the Task name
+                if ($MyInvocation.PipelinePosition -lt $MyInvocation.PipelineLength) {
+                    $currentTask | Write-Output
                 }
             }
-        } else {
-            throw ($errorMessage -f $item, $Name, $Name)
         }
     }
     end {
-        if ($MyInvocation.PipelinePosition -lt $MyInvocation.PipelineLength) {
-            Write-Debug "Sending $Name to pipeline"
-            $Name | Write-Output
-        }
-
-
         Write-Debug "`n$('-' * 80)`n-- End $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
     }
-
 }
