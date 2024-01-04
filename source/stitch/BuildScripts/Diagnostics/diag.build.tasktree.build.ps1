1be7075a2aaa9a199b235ec29236adab9d13c027
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-01-04 16:34:59 -0500

    fix: Get-BuildTask cannot find '*' variable
    - When calling from a function in another module, we need to use $PSCmdlet.GetVariableValue`
      vice getting the variable directly

--- source/stitch/BuildScripts/Diagnostics/diag.build.tasktree.build.ps1
+++ source/stitch/BuildScripts/Diagnostics/diag.build.tasktree.build.ps1
@@ -9,27 +9,9 @@ task diag.build.tasktree {
       - task is a unit of work
       - job an "atomic" unit of work. "do one thing"
     ------------------------------------------------------------------#>
-    $all_tasks = @()
+    Write-Build DarkBlue "Gathering tasks..."
+    $tasks = Get-BuildTask
 
-    foreach ($key in ${*}.All.Keys) {
-        $task = ${*}.All[$key]
-        $task | Add-Member -NotePropertyName Synopsis -NotePropertyValue (Get-BuildSynopsis $task)
-        $task | Add-Member -NotePropertyName phase -NotePropertyValue (( $task.InvocationInfo.InvocationName -like 'phase' ) ? $true : $false)
-        $task | Add-Member -NotePropertyName File -NotePropertyValue (Get-Item $task.InvocationInfo.ScriptName)
-        $task | Add-Member -NotePropertyName Line -NotePropertyValue $task.InvocationInfo.ScriptLineNumber
-        $all_tasks += $task
-    }
-
-    logInfo "A total of $($all_tasks.Count) tasks"
-
-    foreach ( $phase in ($all_tasks | Where-Object -Property phase -EQ $true)) {
-        Write-Build DarkGreen "$($phase.Name) - $($phase.Synopsis)"
-        foreach ($j in $phase.Jobs) {
-            $job = $all_tasks | Where-Object -Property Name -Like $j
-            if ($null -ne $job) {
-                Write-Build White (' - {0,-48} {1}' -f "$($job.Name) ($($job.File.BaseName -replace '\.build$', ''):$($job.Line))", $job.Synopsis)
-            }
-        }
-        Write-Build White ''
-    }
+    Write-Build Gray "Total number of tasks: $($tasks.Count)"
+    Write-Build Gray "$($tasks | Out-String)"
 }
