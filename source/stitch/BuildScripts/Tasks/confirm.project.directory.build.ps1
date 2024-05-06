23e65d4efb940dc6068ada665f870f16bf358015
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-05-06 17:56:42 -0400

    fix(tasks): Create directories idempotently
    - Each directory is checked first to see if it
      exists. It is created only if it does not.
    
    - Better output

--- source/stitch/BuildScripts/Tasks/confirm.project.directory.build.ps1
+++ source/stitch/BuildScripts/Tasks/confirm.project.directory.build.ps1
@@ -1,18 +1,48 @@
+<#
+.SYNOPSIS
+If missing, create the top-level directories in the project
+.DESCRIPTION
+The standard project directories 'Source', 'Staging', 'Tests', 'Artifacts', 'Docs'.  Should be defined in the
+BuildInfo variable, and they should be set to a directory. within the project.
 
-#synopsis: If missing, create the top-level directories in the project
+If they are present, `confirm.project.directory` will skip it without error, if it is not it will be created.
+.NOTES
+---
+@{
+    idempotent = $true
+}
+---
+.COMPONENT
+Validate
+#>
+param()
+
+
 task confirm.project.directory {
+    <# data
+    ---
+    @{
+        idempotent = $true
+    }
+    ---
+    #>
     foreach ($var in @('Source', 'Staging', 'Tests', 'Artifact', 'Docs')) {
-        $path = (Get-Variable $var -ValueOnly)
+        logDebug "Processing info for $var directory"
+        $path = $BuildInfo[$var]
         if ($null -ne $path) {
-            if (Confirm-Path $path) {
-                logInfo (
-                    ' - {0,-16} {1}' -f $path,
-                    ((Get-Item $path) |
-                        Resolve-Path -Relative -ErrorAction SilentlyContinue)
-                )
+            if ($path | Test-Path) {
+                logInfo "Path $var already exists at $path"
             } else {
-                logError "could not create $path"
+                try {
+                    $null = New-Item $path -ItemType Directory
+                    logInfo "Path $var created at $path"
+
+                } catch {
+                    throw "Could not create required project directory $var at $path`n$_"
+                }
             }
+        } else {
+            logError "BuildInfo does not contain a path for $var"
         }
     }
 }
