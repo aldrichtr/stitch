bcf3871eb5dac1d7de89a93d273aa3f79c2e3464
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-05-06 21:23:52 -0400

    task(diag): Add a guard around SourceInfo

--- source/stitch/BuildScripts/Diagnostics/diag.build.configuration.build.ps1
+++ source/stitch/BuildScripts/Diagnostics/diag.build.configuration.build.ps1
@@ -4,7 +4,9 @@ task diag.build.configuration {
     logDebug 'Build Configuration:'
     $config = $BuildInfo
     foreach ($key in $config.Modules.Keys) {
-        $config.Modules[$key].SourceInfo = "Removed for format"
+        if ($null -ne $config.Modules[$key].SourceInfo) {
+            $config.Modules[$key].SourceInfo = "Removed for format"
+        }
     }
     Write-Build Gray ($config | ConvertTo-Psd -Indent 2 | Out-String)
     Write-Build Gray "$('-' * 80)"
